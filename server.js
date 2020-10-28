9fe6b1d4c9914d8db12084473b64f6a6beb29041
Plover Brown <rellwoos@gmail.com>
2020-10-28 14:50:55 -0700

    updating dotenv syntax

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const dotenv = require("dotenv").config();
-
+const { URI } = process.env;
 
 
 const MongoClient = require('mongodb').MongoClient;
